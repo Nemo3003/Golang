124ff1040aaf148cc7163b3b64413714e3a62474
Nemo3003 <ulimarian2000@gmail.com>
2022-09-19 18:18:42 -0300

    [UPD] Guess the number

--- variables/main.go
+++ variables/main.go
@@ -1,12 +1,41 @@
 package main
 
-func main() {
-	var firstNumber int
-	firstNumber = 2
-	// Another way
-	var secondNumber = 5
-	// Thirs way
-	subtraction := 7
+import (
+	"bufio"	
+	"fmt"
+	"os"
+)
 
-	
+const stringRepeat = "and press enter once you are done."
+
+func main() {
+	var firstNumber = 2
+	var secondNumber = 5
+	var subtraction = 7
+	var answer int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	//display a welcome/instructions\
+	fmt.Println("Guess the number game")
+	fmt.Println("----------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10", stringRepeat)
+	reader.ReadString('\n')
+	//Take them through the game
+	fmt.Println("Multiply your number by", firstNumber, stringRepeat)
+	reader.ReadString('\n')
+
+	fmt.Println("Now the result by", secondNumber, stringRepeat)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the name you originally thought of", stringRepeat)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract:", subtraction, stringRepeat)
+	//give them the answer
+
+	answer = firstNumber * secondNumber - subtraction
+	fmt.Println("The answer is", answer)
 }
