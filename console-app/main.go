b7395655f246f0adf02bc0fe09a6a99065fcf305
Nemo3003 <ulimarian2000@gmail.com>
2022-09-20 15:27:30 -0300

    [UPD] Listening to the user's input

--- console-app/main.go
+++ console-app/main.go
@@ -5,6 +5,7 @@ import(
 	"fmt"
 	"log"
 	"github.com/eiannone/keyboard"
+	"strconv"
 )
 
 func main(){
@@ -17,21 +18,34 @@ func main(){
 		_ = keyboard.Close()
 	}()
 
-	fmt.Println("Press any key on the keyboard. Press ESC to quit.")
+	coffees := make(map[int]string)
+	coffees[1] = "Cappuccino"
+	coffees[2] = "Latte"
+	coffees[3] = "Americano"
+	coffees[4] = "Mocha"
+	coffees[5] = "Macchiato"
+	coffees[6] = "Espresso"
+
+	fmt.Println("MENU")
+	fmt.Println("----")
+	fmt.Println("1 - Cappuccino")
+	fmt.Println("2 - Latte")
+	fmt.Println("3 - Americano")
+	fmt.Println("4 - Mocha")
+	fmt.Println("5 - Macchiato")
+	fmt.Println("6 - Espresso")
+	fmt.Println("Q - Quit the program")
 
 	for{
-		char, key, err := keyboard.GetSingleKey()
+		char, _, err := keyboard.GetSingleKey()
 		if err != nil{
 			log.Fatal(err)
 		}
+		i, _ := strconv.Atoi(string(char))
+		fmt.Println(fmt.Sprintf("You chose %s", coffees[i]))
 
-		if key != 0{
-			fmt.Println("You pressed", char, key)
-		}else{
-			fmt.Println("You pressed", char)
-		}
 
-		if key == keyboard.KeyEsc{
+		if char == 'q' || char == 'Q'{
 			break
 		}
 	}
